ddab738890ba479a960809887da2c7d14952e5d2
manasmishra <maverick31@users.noreply.github.com>
2015-09-26 12:50:53 +0530

    Update assignment2_d.sql

--- assignment2/assignment2_d.sql
+++ assignment2/assignment2_d.sql
@@ -1,5 +1,5 @@
 -- Write a SQL statement to count the number of documents containing the word “parliament”
 SELECT count(distinct docid)
 FROM frequency
-where term = 'parliament'
+where term = 'law' or term='legal'
 ;
